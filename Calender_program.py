e3f6348169e65e0882d829314822a64267a5b821
bhavishyapissay77 <70493063+bhavishyapissay77@users.noreply.github.com>
2020-09-18 16:47:38 -0700

    Add files via upload

---
+++ Calender_program.py
@@ -0,0 +1,13 @@
+import calendar
+
+print ("Day:      Time :           Subject:")
+print ("Thursday: 9:10-10:15 AM    Science ")
+print ( "          10:35-11:40 AM   Math"  )
+print ( "          12:10-1:15 PM    P.F")
+print ("Friday:   9:10-10:15 AM    Wheel")
+print ("          10:35-11:40 Am   ELA")
+print ("          12:10-1:15 AM    History")
+
+
+
+
